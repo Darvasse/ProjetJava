2be4b95a0a533c173cc2a61bf38a899b490bef59
Axel KLEIN <axelklein2@gmail.com>
2021-06-07 10:05:09 +0200

    debut view

--- AJAJAJAAJ/app/src/main/java/com/example/projetjava/mvc1/Panel1_Ctrl.java
+++ AJAJAJAAJ/app/src/main/java/com/example/projetjava/mvc1/Panel1_Ctrl.java
@@ -5,12 +5,12 @@ import android.view.View;
 import com.example.projetjava.R;
 
 public class Panel1_Ctrl implements View.OnClickListener {
-    public PnlManager refPnlManager;
+    /*public PnlManager refPnlManager;*/
 
     @Override
     public void onClick(View view) {
         if(view.getId()== R.id.btnGotoPnl2){
-            refPnlManager.setPnl(2);
+            /*refPnlManager.setPnl(2);*/
         }
     }
 }
--- AJAJAJAAJ/app/src/main/java/com/example/projetjava/mvc3/Panel3_View.java
+++ AJAJAJAAJ/app/src/main/java/com/example/projetjava/mvc3/Panel3_View.java
@@ -1,11 +1,26 @@
 package com.example.projetjava.mvc3;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.example.projetjava.R;
+import com.example.projetjava.mvc2.Panel2_Ctrl;
+
 public class Panel3_View extends LinearLayout {
+    public Panel3_Ctrl refCtrl;
+    public Button btnGotoPnl3;
     public Panel3_View(Context context) {
         super(context);
+        LayoutInflater inflater =((Activity)context).getLayoutInflater();
+        inflater.inflate(R.layout.layout_panel3,this);
 
+        btnGotoPnl3=(Button)findViewById(R.id.btnGotoPnl3);
+    }
+    public void setRefCtrl(Panel3_Ctrl c){
+        refCtrl= c;
+        btnGotoPnl3.setOnClickListener(refCtrl);
     }
 }
